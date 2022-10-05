44ce81a484ae23d80413c6527fb60a7cdb453097
Thaqib Mohammed <tmm@T-2.local>
2022-10-04 22:36:47 -0400

    colorschemes

---
+++ init.lua
@@ -0,0 +1,5 @@
+require "userv1.options"
+require "userv1.keymap"
+require "userv1.plugin"
+require "userv1.colorscheme"
+
---
+++ lua/userv1/keymap.lua
@@ -0,0 +1,59 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+
+-- Navigate buffers
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+
+
+-- Move text up and down
+keymap("n", "<S-j>", ":m -2<CR>==", opts)
+keymap("n", "<S-k>", ":m +1<CR>==", opts)
+
+-- Better paste
+keymap("v", "p", '"_dP', opts)
+
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+
