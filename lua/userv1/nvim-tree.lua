fdec8c61b0247da68ef9a4bab3900bcf3e68b6ad
Thaqib Mohammed <thaqibdamani0@gmail.com>
2023-05-11 12:31:38 -0400

    fix nvimtree

--- lua/userv1/nvim-tree.lua
+++ lua/userv1/nvim-tree.lua
@@ -33,17 +33,39 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function my_on_attach(bufnr)
+    local api = require('nvim-tree.api')
+
+    local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set('n', 'O', '', { buffer = bufnr })
+    vim.keymap.del('n', 'O', { buffer = bufnr })
+    vim.keymap.set('n', '<2-RightMouse>', '', { buffer = bufnr })
+    vim.keymap.del('n', '<2-RightMouse>', { buffer = bufnr })
+    vim.keymap.set('n', 'D', '', { buffer = bufnr })
+    vim.keymap.del('n', 'D', { buffer = bufnr })
+    vim.keymap.set('n', 'E', '', { buffer = bufnr })
+    vim.keymap.del('n', 'E', { buffer = bufnr })
+
+    vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+    vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
+    vim.keymap.set('n', 'P', function()
+        local node = api.tree.get_node_under_cursor()
+        print(node.absolute_path)
+    end, opts('Print Node Path'))
+
+    vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
+end
 
 
 nvim_tree.setup {
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {
-        "startify",
-        "dashboard",
-        "alpha",
-    },
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = true,
@@ -70,14 +92,6 @@ nvim_tree.setup {
         width = 30,
         hide_root_folder = false,
         side = "left",
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-                { key = "h", cb = tree_cb "close_node" },
-                { key = "v", cb = tree_cb "vsplit" },
-            },
-        },
         number = false,
         relativenumber = false,
     },
@@ -86,4 +100,5 @@ nvim_tree.setup {
             glyphs = file_icons
         }
     },
+    on_attach = my_on_attach
 }
