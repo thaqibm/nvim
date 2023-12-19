f1f7fca89dcf469ee4106f8cfe363e8a3b51cc4d
Thaqib Mohammed <thaqib.mohammed@snowflake.com>
2023-12-19 11:30:58 -0800

    Add pcalls in init; fix nvimtree

--- init.lua
+++ init.lua
@@ -1,19 +1,32 @@
-require "userv1.options"
-require "userv1.keymap"
-require "userv1.plugin"
-require "userv1.colorscheme"
-require "userv1.cmp"
-require "userv1.lsp"
-require "userv1.treesitter"
-require "userv1.nvimtree"
-require "userv1.bufferline"
-require "userv1.toggleterm"
-require "userv1.knap"
-require "userv1.telescope"
-require "userv1.lualine"
+local modules = {
+    "userv1.options",
+    "userv1.keymap",
+    "userv1.plugin",
+    "userv1.colorscheme",
+    "userv1.cmp",
+    "userv1.lsp",
+    "userv1.treesitter",
+    "userv1.nvimtree",
+    "userv1.bufferline",
+    "userv1.toggleterm",
+    "userv1.knap",
+    "userv1.telescope",
+    "userv1.lualine",
+}
 
--- Disabled plugins:
--- require "userv1.impatient"
+-- Function to log errors in Neovim
+local function log_error(err)
+    vim.api.nvim_err_writeln("Error loading module: " .. err)
+end
+
+-- Load modules using pcall and log errors
+for _, module in ipairs(modules) do
+    local success, err = pcall(require, module)
+    if not success then
+        log_error(err)
+    end
+end
+
 
 local o = vim.opt
 local g = vim.g
